c0fd338322d35dc2cfd026ca90bb2d2f266ee067
m-agour <mo.aggour@gmail.com>
2021-09-13 09:35:37 +0200

    changed port imp

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from website import create_app
 from website.models import User, Problem, Team
 from website import db
+import os
 
+port = int(os.environ.get('PORT', 5000))
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=port)
 
